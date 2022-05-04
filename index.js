ca9d148fef9135b7d1abf0d51c6d1ac944423454
Frank Lee <plat4m@outlook.com>
2022-05-03 22:59:51 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let paraElement = document.getElementById('text');
+    paraElement.textContent = "This is really cool!";
+  });
